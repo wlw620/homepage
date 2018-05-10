49a9464e35fb7f50db4f6de9f43c357512349ad3
毋乐玮 <yuewei.wu@corp.elong.com>
2018-05-10 18:30:38 +0800

    feat: update

--- server/app.js
+++ server/app.js
@@ -1,12 +1,19 @@
 const fs = require('fs');
+const path = require('path');
 const Koa = require('koa');
+const serve = require('koa-static');
+const route = require('koa-route');
+
 const app = new Koa();
 
+const static = serve(path.join(__dirname, '../'));
+console.log(path.join(__dirname, '../'));
 const main = ctx => {
   ctx.response.type = 'html';
-  ctx.response.body = fs.createReadStream('./../index.html');
+  ctx.response.body = fs.createReadStream(path.join(__dirname, '../index.html'));
 };
 
-app.use(main);
+app.use(static);
+app.use(route.get('/', main));
 
-app.listen(443);
+app.listen(3000);
